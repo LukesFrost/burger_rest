fc46c410636f95947693cf12f8c31d33413eae2b
Lukas Forst <forstlukas245@student.voskh.cz>
2026-02-16 11:11:00 +0100

    prvni commit

---
+++ data.js
@@ -0,0 +1,25 @@
+const nabidkaBurgeru = {
+    "classic": {
+        "nazev": "Classic Beef Burger",
+        "cena": 165, },
+    "cheese": {
+        "nazev": "Cheese Burger",
+        "cena": 175, },
+    "bacon": {
+        "nazev": "Bacon Burger",
+        "cena": 185, },
+    "veggie": {
+        "nazev": "Veggie Burger",
+        "cena": 155, },         
+}
+const extraIngredience = {
+    "syr": {
+        "nazev": "Sýr",
+        "priplatek": 15, },
+    "slanina": {
+        "nazev": "Slanina",
+        "priplatek": 15 },
+    "rajce": {
+        "nazev": "Rajče",
+        "priplatek": 10 },                
+}
