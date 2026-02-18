8e3418dbed01362283f61220c38ec85ec068ba50
Lukas Forst <forstlukas245@student.voskh.cz>
2026-02-18 10:57:09 +0100

    for loop na checkboxy

--- data.js
+++ data.js
@@ -1,25 +1,25 @@
 const nabidkaBurgeru = {
     "classic": {
-        "nazev": "Classic Beef Burger",
-        "cena": 165, },
+        nazev: "Classic Beef Burger",
+        cena: 165, },
     "cheese": {
-        "nazev": "Cheese Burger",
-        "cena": 175, },
+        nazev: "Cheese Burger",
+        cena: 175, },
     "bacon": {
-        "nazev": "Bacon Burger",
-        "cena": 185, },
+        nazev: "Bacon Burger",
+        cena: 185, },
     "veggie": {
-        "nazev": "Veggie Burger",
-        "cena": 155, },         
+        nazev: "Veggie Burger",
+        cena: 155, },         
 }
 const extraIngredience = {
     "syr": {
-        "nazev": "Sýr",
-        "priplatek": 15, },
+        nazev: "Sýr",
+        priplatek: 15, },
     "slanina": {
-        "nazev": "Slanina",
-        "priplatek": 15 },
+        nazev: "Slanina",
+        priplatek: 15 },
     "rajce": {
-        "nazev": "Rajče",
-        "priplatek": 10 },                
+        nazev: "Rajče",
+        priplatek: 10 },                
 }
--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const checkbox_form = document.getElementById("ingredients-form");
 
-for (let i = 0; i < extraIngredience.length; i++) {
-    
+for (let typ in extraIngredience) {
+    console.log(extraIngredience[typ].nazev);
+
+    let ing = extraIngredience[typ];
+
+    let label = document.createElement("label");
+    let inp = document.createElement("input");
+    inp.type = "checkbox";
+    inp.name = typ;
+    inp.value = ing.priplatek;
+    inp.className = "checkboxes"
+
+    label.appendChild(inp);
+    label.appendChild(document.createTextNode(` ${ing.nazev} (+${ing.priplatek} Kč)`))
+
+    checkbox_form.appendChild(label);
 }
